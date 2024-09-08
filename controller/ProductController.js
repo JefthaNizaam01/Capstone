bdca7c793bb1541f3ad439c72ec3a9b216a97b83
Nizaam Jeftha <nizaam01.jeftha@gmail.com>
2024-09-08 12:20:34 +0200

    controllerupdate

--- controller/ProductController.js
+++ controller/ProductController.js
@@ -7,27 +7,24 @@ const productRouter = express.Router()
 
 productRouter.use(bodyParser.json())
 
+
 productRouter.get('/', verifyAToken, (req, res) => {
     products.fetchProducts(req, res)
 })
 
-productRouter.get('/recent', (req, res) => {
-    products.recentProducts(req, res)
-})
-
-productRouter.get('/:id',verifyAToken, (req, res) => {
+productRouter.get('/:id', verifyAToken, (req, res) => {
     products.fetchProduct(req, res)
 })
 
-productRouter.post('/add',verifyAToken, (req, res) => {
+productRouter.post('/add', verifyAToken,(req, res) => {
     products.addProduct(req, res)
 })
 
-productRouter.patch('/:id',verifyAToken, (req, res) => {
-    products.updateProduct(req, res)
+productRouter.patch('/:id', verifyAToken, (req, res) => {
+products.updateProduct(req, res)
 })
 
-productRouter.delete('/:id',verifyAToken, (req, res) => {
+productRouter.delete('/:id', verifyAToken, (req, res) => {
     products.deleteProduct(req, res)
 })
 
