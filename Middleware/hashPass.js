37f88cac0da9fe4761bb25dce2c8641522476772
Name <nizaam01.jeftha@gmail.com>
2024-09-12 09:27:48 +0200

    update94

--- Middleware/hashPass.js
+++ Middleware/hashPass.js
@@ -1,20 +1,23 @@
 import bcrypt from 'bcrypt';
 
-const saltRounds = 10;
+import { addUser } from '../Model/db.js';
 
-export const encryptPassword = async (password) => {
-    try {
-        const salt = await bcrypt.genSalt(saltRounds);
-        return await bcrypt.hash(password, salt);
-    } catch (error) {
-        throw new Error('Error encrypting password');
-    }
-};
 
-export const comparePassword = async (password, hashedPassword) => {
-    try {
-        return await bcrypt.compare(password, hashedPassword);
-    } catch (error) {
-        throw new Error('Error comparing password');
-    }
-};
+
+const onHashBefore = (req, res, next) => {
+
+    const {username, emailAdd, passw, userRole, profileURL} = req.body;
+
+    bcrypt.hash(passw, 10, async (err, hashPwd) => {
+
+        if (err) throw err
+        
+        await addUser(username, emailAdd, hashPwd, userRole, profileURL);
+        
+        console.log(`hello ${username}`);
+        
+        next();
+    });
+}
+ 
+export default onHashBefore;
