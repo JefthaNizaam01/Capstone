8284e0c16b3d351ced482d986923752faf971225
Name <nizaam01.jeftha@gmail.com>
2024-09-12 09:52:49 +0200

    update97

--- Controllers/userController.js
+++ Controllers/userController.js
@@ -15,7 +15,7 @@ export default {
             res.send(await getUserByID(+req.params.id))
         } catch (error) {
             res.status(404).json({
-                msg: "Unable to get user that does not exist"
+                msg: "Unable to get a user that does not exist"
             })
         }
     },
@@ -41,7 +41,7 @@ export default {
 
         } catch (error) {
             res.status(404).json({
-                msg: "Unable to delete a user that does not exist"
+                msg: "Unable to delete user that does not exist"
             })
         }
 
